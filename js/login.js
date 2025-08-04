af57a8e47adfac18057fcec8a2ca256d9325a9c1
Ryhan Nalbert <ryhannalbert@gmail.com>
2025-08-04 16:13:50 -0300

    Login page done

--- js/login.js
+++ js/login.js
@@ -1,62 +1,70 @@
-window.localStorage.removeItem('loggedUser')
+window.localStorage.removeItem("loggedUser");
 
-const form = document.querySelector('form');
-const error = document.getElementById('error');
+const form = document.querySelector("form");
+const error = document.getElementById("error");
+const labels = document.querySelectorAll("label");
 
 db = [
-    {
-        username: 'ryhan',
-        password: '2411',
-        role: 'admin'
-    },
-    {
-        username: 'nohan',
-        password: '2411',
-        role: 'user'
-    },
-    {
-        username: 'flavia',
-        password: '2411',
-        role: 'user'
-    }
+  {
+    username: "ryhan",
+    password: "2411",
+    role: "admin",
+  },
+  {
+    username: "nohan",
+    password: "2411",
+    role: "user",
+  },
+  {
+    username: "flavia",
+    password: "2411",
+    role: "user",
+  },
 ];
 generateUsers = () => {
-    localStorage.setItem('users', JSON.stringify(db));
-}
+  localStorage.setItem("users", JSON.stringify(db));
+};
 generateUsers();
 
-data = JSON.parse(localStorage.getItem('users'));
+data = JSON.parse(localStorage.getItem("users"));
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    if (data.find(user => user.username === username && user.password === password)) {
+  if (
+    data.find(
+      (user) => user.username === username && user.password === password
+    )
+  ) {
+    console.log(`Usuário ${username} logado com sucesso!`);
 
-        console.log(`Usuário ${username} logado com sucesso!`);
-        
-        userData = [data.find(user => user.username === username)]
-        localStorage.setItem('loggedUser', JSON.stringify(userData))
+    userData = [data.find((user) => user.username === username)];
+    localStorage.setItem("loggedUser", JSON.stringify(userData));
 
-        window.location.href = '../pages/index.html';
+    window.location.href = "../pages/index.html";
+  } else if (username === "" || password === "") {
+    error.innerHTML = "Preencha todos os campos!";
+    error.style.display = "block";
 
-    } else if (username === '' || password === '') {
+    setTimeout(() => {
+      error.style.display = "none";
+    }, 3000);
+  } else {
+    error.innerHTML = "Usuário ou senha inválido!";
+    error.style.display = "block";
 
-        error.innerHTML = 'Preencha todos os campos!';
-        error.style.display = 'block';
+    setTimeout(() => {
+      error.style.display = "none";
+    }, 3000);
+  }
+});
 
-        setTimeout(() => {
-            error.style.display = 'none';
-        }, 3000)
-        
-    } else {
-        error.innerHTML = 'Usuário ou senha inválido!'
-        error.style.display = 'block';
-        
-        setTimeout(() => {
-            error.style.display = 'none';
-        }, 3000)
-    }
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map((l, i) => `<span style="transition-delay: ${i * 50}ms;">${l}</span>`)
+    .join("")
 });
